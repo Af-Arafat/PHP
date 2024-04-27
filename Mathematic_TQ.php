94961ecc2e150425b2ca8527c13cf4befa2f5ce1
Af-Arafat <ikramkhan6947@gmail.com>
2024-04-27 15:00:27 +0600

    new file

--- Mathematic_TQ.php
+++ Mathematic_TQ.php
@@ -1,14 +1,29 @@
 <?php
-$num = 12;
-$num = $num + 13;
-echo $num ,"\n";
+// $num = 12;
+// $num = $num + 13;
+// echo $num ,"\n";
 
 
+// // 7 More on addition subtraction
+// $n = 7;
+// $m = $n++; // $m = $++n
+// echo $m, "\n", $n;
 
-$n = 7;
-$m = $n++;
-echo $m, "\n", $n;
-
+/*
 $m = $n++;
 $m = $n;
 $n = $n + 1;
+*/
+
+/*
+$m = ++$n;
+$n = $n + 1;
+$m = $n;
+*/
+
+// // 8 Discussion of different number systems
+
+// $a = 123;
+// $b = 012;
+
+// printf("The Number is %d and %d", $a, $b);
