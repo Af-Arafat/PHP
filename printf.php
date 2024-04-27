097e4ce87f69b185939d99a6dd148055f0da5e87
Af-Arafat <ikramkhan6947@gmail.com>
2024-04-27 15:47:05 +0600

    new code

--- printf.php
+++ printf.php
@@ -8,3 +8,14 @@ echo "\n";
 
 // printf("The binary equivalent of %d is %b", 12, 12);
 printf('The binary equivalent of %1$d is %1$b', 12);
+echo "\n";
+
+$n = 45.234;
+printf("%.2f", $n);
+echo "\n";
+
+$m = 123; //0123
+$n = 12; //0012
+
+printf("%04d \n", $m);
+printf("%04d \n", $n);
