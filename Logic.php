3b5984e0ffdaf081c48437215d14cd7c52fd2d58
Af-Arafat <ikramkhan6947@gmail.com>
2024-04-29 17:21:07 +0600

    new test

--- Logic.php
+++ Logic.php
@@ -1,11 +1,22 @@
 <?php
-$n = 13;
-if($n %2 == 0){
-    echo "$n It's enven number";
-}else{
-    echo "$n is odd number";
-}
+// $n = 13;
+// if($n %2 == 0){
+//     echo "$n It's enven number";
+// }else{
+//     echo "$n is odd number";
+// }
 
-if ($n >10){
-    echo "$n is greater then 10";
+// if ($n >10){
+//     echo "$n is greater then 10";
+// }
+
+$year = 2004;
+if($year % 4 == 0 && $year % 100 == 0 && $year % 400 ==0){
+    echo "{$year} is a leap year";
+}elseif ($year %4 == 0 && $year % 100 == 0){
+    echo "{$year} is not a leap year";
+}elseif($year %4 == 0){
+    echo "{$year} is a leap year";
+}else{
+    echo "{$year} is not a leap year";
 }
