196e6ce10b54b5e5cbe6bf7922f89ef6f9f2132c
Af-Arafat <ikramkhan6947@gmail.com>
2024-04-29 17:08:59 +0600

    test

---
+++ Logic.php
@@ -0,0 +1,11 @@
+<?php
+$n = 13;
+if($n %2 == 0){
+    echo "$n It's enven number";
+}else{
+    echo "$n is odd number";
+}
+
+if ($n >10){
+    echo "$n is greater then 10";
+}
--- printf.php
+++ printf.php
@@ -6,6 +6,10 @@ $mname = "Apple";
 printf('His name is %2$s %3$s %1$s', $lname, $fname, $mname);
 echo "\n";
 
+$output = sprintf('His name is %2$s %3$s %1$s', $lname, $fname, $mname);
+echo $output;
+echo "\n";
+
 // printf("The binary equivalent of %d is %b", 12, 12);
 printf('The binary equivalent of %1$d is %1$b', 12);
 echo "\n";
